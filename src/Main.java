4fcc79c789f4299a86ff24176232a2ffbe822fd9
Professional <new99kpicem@gmail.com>
2023-02-27 01:04:39 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        task1();
+        //task2();
+       // task3();
+        //task4();
 
-}
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = new int[] {1, 2, 3};
+        System.out.println(Arrays.toString(arr));
+        double[] fraction = new double[] {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(fraction));
+        char[] my = new char[] {9403, 9418, 9400, 9408};
+        System.out.println(Arrays.toString(my));
+        }
+        }
+
